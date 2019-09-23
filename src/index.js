7774efa8b7911f654fb7a7979b72579a0f9c7a38
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-09-23 12:06:25 -0500

    #26 Exportación de DummySmall

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ export { default as DashboardBase } from "./base/DashboardBase";
 
 /* --- DUMMY COMPONENTS --- */
 
+export { default as DummySmall } from "./dummy/DummySmall";
 export { default as DummyMedium } from "./dummy/DummyMedium";
