053954fc302768da392c58840fdc244a54b00ed6
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-11-21 14:50:53 -0500

    Update PaperBase.tsx

--- src/common/PaperBase.tsx
+++ src/common/PaperBase.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Paper } from "@material-ui/core";
 import { MaterialBase } from "src/MaterialBase";
+import { Children } from "src/Children";
 
 interface Props extends MaterialBase {
-  children?: JSX.Element[] | JSX.Element | string;
+  children?: Children;
 }
 
 export default function PaperBase(props: Props) {
