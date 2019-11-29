e6d7ea0f645c4ead20fcdc4078a11965b74b9c3a
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-11-29 15:26:41 -0500

    #48 Fix de compilación: import obsoleto

--- src/types/AppBarBaseProps.d.ts
+++ src/types/AppBarBaseProps.d.ts
@@ -1,6 +1,5 @@
 import { MaterialBase } from "src/types/MaterialBase";
 import { Children } from "src/types/Children";
-import { HideOnScrollProps } from "../drawer/HideOnScrollProps";
 
 export interface AppBarBaseProps extends Children, MaterialBase {
   classes: Record<string, string>;
