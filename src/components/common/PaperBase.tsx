c7b79c7505baf5a385f484149325539d7d15d904
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-12-10 17:00:43 -0500

    #57 Se añaden todos los props nativos de paper

--- src/components/common/PaperBase.tsx
+++ src/components/common/PaperBase.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Paper } from "@material-ui/core";
-import { Children } from "../../types/Children";
-
-interface PaperBaseProps extends Children {}
+import { PaperBaseProps } from "../../types/PaperBaseProps";
 
 /** Componente base que implementa el componente Paper
  * nativo de material-ui.
  *
- * @param props IconButtonBaseProps
+ * @param props PaperBaseProps
  * @returns JSX.Element
  */
 export default function PaperBase(props: PaperBaseProps) {
-  const { children } = props;
-  return <Paper>{children}</Paper>;
+  const { square, elevation, component, children } = props;
+
+  return (
+    <Paper square={square} elevation={elevation} component={component}>
+      {children}
+    </Paper>
+  );
 }
--- src/types/PaperBaseProps.d.ts
+++ src/types/PaperBaseProps.d.ts
@@ -1,8 +1,18 @@
 import { Children } from "./Children";
+import { ElementType } from "react";
 
 /** Interfaz que define las propiedades que debe
  * tener el componente "PaperBase".
  *
  * @see https://material-ui.com/api/paper/
  */
-export interface PaperBaseProps extends Children {}
+export interface PaperBaseProps extends Children {
+  /** Si es true, las esquinas redondeadas se desactivan. */
+  square?: boolean;
+
+  /** Produndidad de las sombras bajo el elemento. Acepta valores entre 0 y 24. */
+  elevation?: number;
+
+  /** Componente usado en la raíz de nodo. */
+  component?: React.ElementType<React.HTMLAttributes<HTMLElement>>;
+}
