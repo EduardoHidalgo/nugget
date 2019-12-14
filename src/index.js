7cd7256f8e9e4720c8ee0a93a08c5979d3897346
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-12-14 15:54:27 -0500

    Se añaden los componentes Button, ButtonGroup y FabButton.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ export { default as Module } from "./components/module/Module";
 /* --- COMMON --- */
 
 export { default as Container } from "./components/common/ContainerBase";
-export { default as IconButton } from "./components/common/IconButtonBase";
 export { default as Paper } from "./components/common/PaperBase";
 export { default as Text } from "./components/common/TextBase";
 export { default as ExpansionPanel } from "./components/expansionPanel/ExpansionPanelBase";
+export { default as Button } from "./components/common/ButtonBase";
+export { default as ButtonGroup } from "./components/common/ButtonGroupBase";
+export { default as FabButton } from "./components/common/FabButtonBase";
+export { default as IconButton } from "./components/common/IconButtonBase";
 
 /* --- UTILS --- */
 
